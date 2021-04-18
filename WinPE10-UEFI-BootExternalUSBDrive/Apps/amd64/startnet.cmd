081a66a0eb91b6a47fa3bbffb6ee6780c9eb6ec9
Dede333 <61848651+Dede333@users.noreply.github.com>
2021-04-18 10:56:24 +0200

    Delete WinPE10-UEFI-BootExternalUSBDrive directory

--- WinPE10-UEFI-BootExternalUSBDrive/Apps/amd64/startnet.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-wpeinit.exe /unattend=X:\Windows\System32\WinPEResolution.xml
-cd X:\Windows\system32
-powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
-Start Powershell
-powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -File X:\Windows\system32\WinPE-WIM_FFU.ps1
-CLS
-GoTo:EOF
